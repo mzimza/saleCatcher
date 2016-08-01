f5478c9dc164d888811f05609c307dc8b89737f4
Maja <maja.zalewska@gmail.com>
2016-08-01 15:42:43 +0200

    Initial Commit

---
+++ saleCatcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  saleCatcher
+//
+//  Created by Maja Zalewska on 01/08/16.
+//  Copyright © 2016 Maja Zalewska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
